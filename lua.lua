2a39773383c0f80e7cbeda5c9aa253e988ae6509
takecare <rui@vazteixeira.org>
2016-05-03 22:09:11 +0100

    clean up

--- lua.lua
+++ lua.lua
@@ -11,6 +11,7 @@ print '\n\n*** VARS & FLOW ***'
 
 num = 42 -- every number is a 64bit-precision double
 
+print '\n___ declaring variables'
 name = 'rui' -- strings are immutable
 wall_of_text = [[   lorem
                     ipsum
@@ -23,6 +24,7 @@ print(wall_of_text)
 
 wall_of_text = nil -- wall_of_text can now be gc'ed
 
+print '\n___ flow control and scope'
 i = 0
 while i < 5 do
     print(i)
@@ -35,7 +37,7 @@ do -- you can define blocks like this
     name = 'miguel'
 end
 
-print(age) -- not defined here...
+print('printing a var out of current scope: ' .. tostring(age)) -- not defined here, should print nil
 
 if i == 0 then
     print("i hasn't changed...")
@@ -44,28 +46,29 @@ elseif name ~= 'rui' then -- not equals
     bool = true -- bool is declared as a global
 else
     io.write('dang it... what\'s the name, then? ')
-    input = io.read() -- this is global!
+    input = io.read() -- this is global too!
 end
 
 if bool then
-    print(input)
+    print(input) -- it's nil at this point
 elseif not bool then
     print("REDUNDANCY!")
 end
 
-for i = 1, 5 do print(i); i = i + 1 end
-for j = 10, 1, -2 do print(j) end -- 'j' is local to the for loop
-
-repeat input = io.read() until input ~= 'functions'
+count = 0
+repeat
+    io.write(count .. ' please type \'takecare\': ')
+    input, count = io.read(), count + 1 -- multiple assignment!
+until input == 'takecare'
 
 
 -- *****************
 -- *** FUNCTIONS ***
 print '\n\n*** FUNCTIONS ***'
 
-function fizzBuzz(n)
+function fizzBuzz(n) -- you can improve this ;)
     if n % 5 == 0 and n % 3 == 0 then
-        print("fizzbuzz") -- no str concatenation yet! :P
+        print("fizzbuzz")
     elseif n % 3 == 0 then
         print("fizz")
     elseif n % 5 == 0 then
@@ -84,15 +87,17 @@ function startFrom(y) -- anonymous function! remembers value of 'y'
     return f
 end
 
+print '___ anonymous functions'
 addToAHundred = startFrom(100)
 print(addToAHundred(10))
+
 addToAThousand = startFrom(1000)
 print(addToAThousand(50))
 
 local x, y, z = -200, 400 -- z is nil!
-print(addToAHundred(x,400)) -- 400 is discarded!
+print(addToAHundred(x,400)) -- 400 is discarded 'cause addToAHundred takes only 1 argument
 
-function call(f, times)
+function call(f, times) -- passing a function as an argument
     for k = 1, times do f() end
 end
 
@@ -102,9 +107,10 @@ end
 
 call(dumb,5)
 
+-- can  you guess the outcome?
 local g = function (x) print(x); return function (y) print(y); return math.sin(y) * math.cos(x) end end
 local v = g(10)(10)
-print(v)
+io.write('and the value is... ' .. v)
 
 
 -- **************
@@ -112,6 +118,7 @@ print(v)
 -- lua's only data structure, it's essentially a map
 print '\n\n*** TABLES ***'
 
+print '___ declaring tables, the only data structure lua has!'
 table = { name = 'rui', age = 42 } -- the keys are like strings
 age = table['age']
 print(table.name .. ' is ' .. age .. ' years old')
@@ -120,26 +127,28 @@ person = { name = 'nando', age = '23', height = 180 }
 person.weight = 73 -- adds a new entry to the table
 person.height = nil -- removes an entry from the table
 
-pets = { 'cat', 'dog', 'fish', 'snake' } -- tables with no defined keys work like arrays
-weights = { 42.3, 74, 68.7 }
+print '___ using tables as arrays'
+pets = { 'cat', 'dog', 'fish', 'snake' } -- tables with no defined keys work like arraysweights = { 42.3, 74, 68.7 }
 print(pets[1]) -- 'array' indexes start at 1
 pets[-1] = 'tardigrad' -- and given that they're tables, they can have negative indexes
 pets[100] = 't-rex' -- and holes!
 
+print '___ there are different ways to access a table\'s values'
 table = { key = 'value' }
 aKey = 'key'
 anotherKey = 'k' .. 'e' .. 'y'
 print(table[aKey] .. ' ' .. table[anotherKey])
 
+
 table = {['+'] = "plus", ['-'] = "minus", ['='] = "equals", [{}] = "empty"}
-if table[{}] then -- this {} is a different object from the one in the table
+if table[{}] then -- this {} is a different object from the one in the table, it's a new one!
     print("found!")
 else
-    print("not found!") 
+    print("not found!")
 end
 
 function printTable(table)
-    for key, value in pairs(table) do
+    for key, value in pairs(table) do -- pairs() is really handy
         print(key, value)
     end    
 end
@@ -150,8 +159,15 @@ function printList(list)
     end    
 end
 
+function anotherPrintList( ... )
+    for i, v in ipairs(list) do
+        print(i .. ' ' .. v)
+    end
+end
+
 printTable(_G.pets) -- _G is a table that contains all globals
---print(getmetatable(_G))
+--printList(pets) -- won't work 'cause this list has wholes
+--anotherPrintList(pets) -- won't work 'cause this list has wholes
 
 
 -- ******************
@@ -192,6 +208,6 @@ printTable(k)
 pC = pA + pB
 -- printTable(pC)
 
-pX = pC.scale(pC,3)
+-- pX = pC.scale(pC,3)
 
 print(#pA)
